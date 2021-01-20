d3406316bf7b8b9674dbb63c031921fbd7d73531
Igor Michels <igorxadrezbr@gmail.com>
2021-01-20 10:05:31 -0300

    organize

--- functions.py
+++ functions.py
@@ -87,3 +87,157 @@ def catch_data(all_data):
     CBF = all_data[end_player - 5:end_player + 1]
     
     return CBF, apelido, nome, no, end_player
+
+def find_players(file):
+    with open(file) as output_file:
+        fplayer = False
+        players = [['CBF', 'Apelido', 'Nome', 'Nº', 'Clube']]
+        reader = csv.reader(output_file, delimiter = ',')
+        error = 0
+        for line in reader:
+            if 'Relação de Jogadores' in line:
+                fplayer = True
+                cont = 0
+
+            if fplayer:
+                try:
+                    int(line[0])
+                except:
+                    try:
+                        int(line[5][0])
+                    except:
+                        error += 1
+                
+                if error > 3:
+                    break
+                
+                if cont == 1:
+                    team1, team2 = teams(line)
+                elif cont > 2:
+                    if len(line) == 12:
+                        CBF = line[5]
+                        apelido = line[1]
+                        nome = line[2]
+                        no = line[0]
+                        clube = team1
+                        players.append([CBF, apelido, nome, no, clube])
+                        CBF = line[11]
+                        apelido = line[7]
+                        nome = line[8]
+                        no = line[6]
+                        clube = team2
+                        players.append([CBF, apelido, nome, no, clube])
+                    else:
+                        if line[0] == '':
+                            clube = team2
+                        else:
+                            clube = team1
+                        
+                        all_data = list2string(list2string(line).split())
+                        CBF, apelido, nome, no, end_player = catch_data(all_data)
+                        players.append([CBF, apelido, nome, no, clube])
+                        if clube == team1 and end_player != len(all_data) - 1:
+                            clube = team2
+                            all_data = all_data[end_player + 2:]
+                            CBF, apelido, nome, no, end_player = catch_data(all_data)
+                            players.append([CBF, apelido, nome, no, clube])
+                            
+                cont += 1
+
+    return players
+
+def list2string2(line):
+    string = line[0]
+    for i in range(1, len(line)):
+        if line[i] != '':
+            string += ' ' + line[i]
+            
+    return string
+
+def find_game_players(file):
+    with open(file) as output_file:
+        subs = False
+        changes = [['CBF', 'In', 'Out', 'Nº', 'Clube']]
+        reader = csv.reader(output_file, delimiter = ',')
+        error = 0
+        players = find_players(file)
+        for player in players[1:23]:
+            player[1] = '00:00 - 1T'
+            player[2] = '45:00 - 2T'
+            changes.append(player)
+            
+        for line in reader:
+            if 'Substituições' in line:
+                subs = True
+                cont = 0
+
+            if subs:
+                try:
+                    int(line[0][0])
+                except:
+                    try:
+                        int(line[5][0])
+                    except:
+                        error += 1
+                
+                if error > 2:
+                    break
+                
+                if cont > 1:
+                    if len(line) == 5:
+                        if line[1] == 'INT':
+                            time = 'INT'
+                        else:
+                            time = line[0] + ' - ' + line[1]
+                        clube = line[2]
+                        player_in = line[3]
+                        player_out = line[4]
+                        if player_in[1] == ' ':
+                            no_in = player_in[0]
+                        else:
+                            no_in = player_in[:2]
+                        
+                        if player_out[1] == ' ':
+                            no_out = player_out[0]
+                        else:
+                            no_out = player_out[:2]
+                    else:
+                        all_data = list2string2(line)
+                        tp = find_carac(all_data, ':')
+                        if all_data[tp + 4] == 'I':
+                            time = 'INT'
+                        else:
+                            time = all_data[:tp + 3] + ' - ' + all_data[tp + 4: tp + 6]
+                        
+                        end_clube = find_carac(all_data[tp + 6:], '0123456789') - 1
+                        if time == 'INT':
+                            clube = all_data[tp + 8:tp + 6 + end_clube]
+                        else:
+                            clube = all_data[tp + 7:tp + 6 + end_clube]
+                            
+                        if all_data[tp + 6 + end_clube + 2] == ' ':
+                            no_in = all_data[tp + 6 + end_clube + 1]
+                        else:
+                            no_in = all_data[tp + 6 + end_clube + 1:tp + 6 + end_clube + 3]
+                        
+                        bn = find_carac(all_data[tp + 6 + end_clube + 3:], '0123456789')
+                        
+                        if all_data[tp + 6 + end_clube + 3 + bn + 1] == ' ':
+                            no_out = all_data[tp + 6 + end_clube + 3 + bn]
+                        else:
+                            no_out = all_data[tp + 6 + end_clube + 3 + bn:tp + 6 + end_clube + 3 + bn + 2]
+                        
+                    for player in players:
+                        if player[3] == no_in and player[4] == clube:
+                            player[1] = time
+                            player[2] = '45 - 2T'
+                            changes.append(player)
+
+                    for player in changes:
+                        if player[3] == no_out and player[4] == clube:
+                            player[2] = time
+                        
+                        
+                cont += 1
+
+    return changes
